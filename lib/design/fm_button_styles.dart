701f67febdf557dd6636c7acea2ec13597a1657f
Djuro Lecic <lecicdjuro@gmail.com>
2021-10-01 09:09:21 +0200

    Self review finished (#8)
    self review

--- lib/design/fm_button_styles.dart
+++ lib/design/fm_button_styles.dart
@@ -1,19 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:fm_music/design/fm_colors.dart';
 
-class FmButtonStyles{
-
-
-  static ButtonStyle primaryButtonStyle({Color color = FmColors.primaryButton}) {
-    return
-      ElevatedButton.styleFrom(
-        onPrimary: Colors.black87,
-        primary: Colors.grey[300],
-        minimumSize: const Size(88, 36),
-        padding: const EdgeInsets.symmetric(horizontal: 16),
-        shape: const RoundedRectangleBorder(
-          borderRadius: BorderRadius.all(Radius.circular(2)),
-        ),
-      );
+class FmButtonStyles {
+  static ButtonStyle primaryButtonStyle(
+      {Color color = FmColors.primaryButton}) {
+    return ElevatedButton.styleFrom(
+      onPrimary: Colors.black87,
+      primary: Colors.grey[300],
+      minimumSize: const Size(88, 36),
+      padding: const EdgeInsets.symmetric(horizontal: 16),
+      shape: const RoundedRectangleBorder(
+        borderRadius: BorderRadius.all(Radius.circular(2)),
+      ),
+    );
   }
 }
